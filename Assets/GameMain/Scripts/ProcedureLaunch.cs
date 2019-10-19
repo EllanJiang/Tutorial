723772d639ee2d3727fc1a7056e982b69530d098
Jiang Yin <jiangyin@gameframework.cn>
2019-10-19 16:54:02 +0800

    增加插件默认场景

---
+++ Assets/GameMain/Scripts/ProcedureLaunch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using GameFramework;
+using GameFramework.Event;
+using GameFramework.Procedure;
+using UnityGameFramework.Runtime;
+using ProcedureOwner = GameFramework.Fsm.IFsm<GameFramework.Procedure.IProcedureManager>;
+
+namespace Tutorial
+{
+    public class ProcedureLaunch : ProcedureBase
+    {
+        // 游戏初始化时执行。
+        protected override void OnInit(ProcedureOwner procedureOwner)
+        {
+            base.OnInit(procedureOwner);
+        }
+
+        // 每次进入这个流程时执行。
+        protected override void OnEnter(ProcedureOwner procedureOwner)
+        {
+            base.OnEnter(procedureOwner);
+
+            string welcomeMessage = Utility.Text.Format("Hello! This is an empty project based on Game Framework {0}.", Version.GameFrameworkVersion);
+            Log.Info(welcomeMessage);
+            Log.Warning(welcomeMessage);
+            Log.Error(welcomeMessage);
+
+            EventComponent eventComponent = GameEntry.GetComponent<EventComponent>();
+            EventHandler<GameEventArgs> loadDataTableSuccess = OnLoadDataTableSuccess;
+            eventComponent.Subscribe(LoadDataTableSuccessEventArgs.EventId, loadDataTableSuccess);
+            eventComponent.Unsubscribe(LoadDataTableSuccessEventArgs.EventId, loadDataTableSuccess);
+        }
+
+        /// <summary>
+        /// 加载数据表成功事件响应函数。
+        /// </summary>
+        /// <param name="sender">事件发送者。</param>
+        /// <param name="e">事件。</param>
+        private void OnLoadDataTableSuccess(object sender, GameEventArgs e)
+        {
+            LoadDataTableSuccessEventArgs ne = (LoadDataTableSuccessEventArgs)e;
+        }
+
+        // 每次轮询执行。
+        protected override void OnUpdate(ProcedureOwner procedureOwner, float elapseSeconds, float realElapseSeconds)
+        {
+            base.OnUpdate(procedureOwner, elapseSeconds, realElapseSeconds);
+        }
+
+        // 每次离开这个流程时执行。
+        protected override void OnLeave(ProcedureOwner procedureOwner, bool isShutdown)
+        {
+            base.OnLeave(procedureOwner, isShutdown);
+        }
+
+        // 游戏退出时执行。
+        protected override void OnDestroy(ProcedureOwner procedureOwner)
+        {
+            base.OnDestroy(procedureOwner);
+        }
+    }
+}
