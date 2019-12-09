178fc2ef421041f9a4ea08f9e094999d6b075eb6
Jiang Yin <jiangyin@gameframework.cn>
2019-12-09 14:38:59 +0800

    第二章：Hello World

--- Assets/GameMain/Scripts/ProcedureLaunch.cs
+++ Assets/GameMain/Scripts/ProcedureLaunch.cs
@@ -18,10 +18,22 @@ namespace Tutorial
         {
             base.OnEnter(procedureOwner);
 
-            string welcomeMessage = Utility.Text.Format("Hello! This is an empty project based on Game Framework {0}.", Version.GameFrameworkVersion);
+            string welcomeMessage = Utility.Text.Format("Hello Game Framework {0}.", Version.GameFrameworkVersion);
+
+            // 打印调试级别日志，用于记录调试类日志信息
+            Log.Debug(welcomeMessage);
+
+            // 打印信息级别日志，用于记录程序正常运行日志信息
             Log.Info(welcomeMessage);
+
+            // 打印警告级别日志，建议在发生局部功能逻辑错误，但尚不会导致游戏崩溃或异常时使用
             Log.Warning(welcomeMessage);
+
+            // 打印错误级别日志，建议在发生功能逻辑错误，但尚不会导致游戏崩溃或异常时使用
             Log.Error(welcomeMessage);
+
+            // 打印严重错误级别日志，建议在发生严重错误，可能导致游戏崩溃或异常时使用，此时应尝试重启进程或重建游戏框架
+            Log.Fatal(welcomeMessage);
         }
 
         // 每次轮询执行。
